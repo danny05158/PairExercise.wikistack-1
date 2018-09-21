66dfcebef2253587c9b9c453f885f64097b7d876
Daniel Casasola <casasolad86@gmail.com>
2018-09-21 11:55:44 -0500

    slug route

--- routes/wiki.js
+++ routes/wiki.js
@@ -4,14 +4,26 @@ const router = express.Router();
 
 const { Page } = require('../models');
 const { addPage } = require('../views');
+const  wikipage  = require('../views/wikipage')
 
 router.get('/', (req, res) => {
   res.send('got to GET /wiki/');
 });
 
+
 router.get('/add', (req, res) => {
   res.send(addPage());
 });
+
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const foundPage = await Page.findOne({
+      where: {slug: req.params.slug}
+    })
+    res.send(wikipage(foundPage, foundPage.author))
+
+  } catch (err){ next(err)}
+})
 
 router.post('/', async (req, res, next) => {
   const page = new Page({
@@ -22,7 +34,7 @@ router.post('/', async (req, res, next) => {
 
   try {
     await page.save();
-    res.redirect('/');
+    res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error);
   }
